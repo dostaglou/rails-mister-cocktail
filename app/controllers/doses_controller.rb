1e6faaecd60fa5ff42f7d8b8ecf47990085124a7
dillon wyatt <dostaglou@gmail.com>
2019-08-08 16:26:58 +0900

    Now to style

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+  before_action :cocktail_id, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @does.destroy
+    redirect_to cocktail_path(@cocktail)
+
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:dose])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def cocktail_id
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  end
