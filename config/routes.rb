2b1c83c1ae926bb3bce4b55d51b6fadd5e4ad230
dillon wyatt <dostaglou@gmail.com>
2019-08-09 12:25:05 +0900

    tried to exclude resources index route for root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails do
+  resources :cocktails, except: :index do
     resources :doses, only: [:new, :create]
   end
 
